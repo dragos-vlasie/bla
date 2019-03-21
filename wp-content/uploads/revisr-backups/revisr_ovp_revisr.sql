5d36966398f7889a22f7ff3e578d803ab0f9a624
dragos.vlasie@thisplace.com <dragos.vlasie@thisplace.com>
2019-03-21 16:29:02 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_ovp_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_ovp_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `ovp_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `ovp_revisr` WRITE;
 /*!40000 ALTER TABLE `ovp_revisr` DISABLE KEYS */;
-INSERT INTO `ovp_revisr` VALUES (1,'2019-03-20 07:48:47','Successfully created a new repository.','init','dragos.vlasie@thisplace.com'),(2,'2019-03-20 07:49:01','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(3,'2019-03-20 07:50:28','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(4,'2019-03-20 07:56:46','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=af6183a&success=true\">#af6183a</a> to the local repository.','commit','dragos.vlasie@thisplace.com'),(5,'2019-03-20 08:21:43','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(6,'2019-03-20 08:23:04','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(7,'2019-03-20 08:48:20','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=c556efd&success=true\">#c556efd</a> to the local repository.','commit','dragos.vlasie@thisplace.com'),(8,'2019-03-20 08:49:45','Successfully pushed 1 commit to origin/master.','push','dragos.vlasie@thisplace.com');
+INSERT INTO `ovp_revisr` VALUES (1,'2019-03-20 07:48:47','Successfully created a new repository.','init','dragos.vlasie@thisplace.com'),(2,'2019-03-20 07:49:01','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(3,'2019-03-20 07:50:28','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(4,'2019-03-20 07:56:46','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=af6183a&success=true\">#af6183a</a> to the local repository.','commit','dragos.vlasie@thisplace.com'),(5,'2019-03-20 08:21:43','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(6,'2019-03-20 08:23:04','Error pushing changes to the remote repository.','error','dragos.vlasie@thisplace.com'),(7,'2019-03-20 08:48:20','Committed <a href=\"http://localhost:8888/wp-admin/admin.php?page=revisr_view_commit&commit=c556efd&success=true\">#c556efd</a> to the local repository.','commit','dragos.vlasie@thisplace.com'),(8,'2019-03-20 08:49:45','Successfully pushed 1 commit to origin/master.','push','dragos.vlasie@thisplace.com'),(9,'2019-03-21 09:28:58','Successfully imported the database. ','import','dragos.vlasie@thisplace.com');
 /*!40000 ALTER TABLE `ovp_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
